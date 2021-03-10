9f606283954a5c1976c2f2b502b5adfd0784df94
Avijit saha <avijitsahashovo2016@gmail.com>
2021-03-11 01:30:20 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import League from './components/League/League';
 import LeagueDetail from './components/LeagueDetail/LeagueDetail';
 import Footer from './components/Footer/Footer';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
+    <Router>
     <div className="App">
+    
       <Header></Header>
       <div className="container">
         <div className="row">
+        <Switch>
+          <Route exact path="/">
           <League></League>
           <League></League>
           <League></League>
+          </Route>
+          <Route path="/details">
+          <LeagueDetail></LeagueDetail>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
         </div>
-        <LeagueDetail></LeagueDetail>
       </div>
-      <br/>
-      <Footer></Footer>
+        <Footer></Footer>
     </div>
+      
+
+    </Router>
   );
 }
 
--- src/components/League/League.js
+++ src/components/League/League.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import image from '../../assets/images/image8.png';
 import './League.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 const League = () => {
-    return (
-        <div className="col-md-4">
-            <div className="card card-height">
-            <img src={image} className="card-img-top card-image" alt="" />
-            <div className="card-body">
-              <h3 className="card-title">Card title</h3>
-              <p className="card-text">This is a wider card with supporting text below as a natural  This content is a little bit longer.</p>
-            </div>
-            <div className="card-footer">
-              <button className="btn-explore btn">Explore <FontAwesomeIcon icon={faArrowAltCircleRight} /></button>
-              </div>
+  return (
+      <div className="col-md-4">
+        <div className="card card-height">
+          <img src={image} className="card-img-top card-image" alt="" />
+          <div className="card-body">
+            <h3 className="card-title">Card title</h3>
+            <p className="card-text">This is a wider card with supporting text below as a natural  This content is a little bit longer.</p>
+          </div>
+          <div className="card-footer border-0 bg-white">
+            <Link to="/details">
+              <button className="btn-explore btn">Explore
+              <FontAwesomeIcon icon={faArrowAltCircleRight} />
+              </button>
+            </Link>
           </div>
         </div>
-    );
+      </div>
+
+  );
 };
 
 export default League;
